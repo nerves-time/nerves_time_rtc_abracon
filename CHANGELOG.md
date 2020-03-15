82e8d2f9afc27aa5c5c9b9d6213e631e1e0b5296
Frank Hunleth <fhunleth@troodon-software.com>
2020-03-15 12:53:22 -0400

    v0.1.1 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## v0.1.1
+
+* Bug fixes
+  * Improve error return when the RTC isn't found
+
 ## v0.1.0
 
 Initial release
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule NervesTime.RTC.Abracon.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @source_url "https://github.com/nerves-time/nerves_time_rtc_abracon"
 
   def project do
